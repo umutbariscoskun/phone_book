16155940c4de2fcc41d742ac830a507422d09a43
Umut <umutcoskun2017@gmail.com>
2022-03-14 23:15:11 +0300

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:phone_book/src/app/pages/home/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
